c8e03ff8d646379544d35174942fab4bb101e8c7
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-07-09 21:05:24 -0300

    Done

--- server/server.js
+++ server/server.js
@@ -10,13 +10,18 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Apollo 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
-server.applyMiddleware({ app });
+startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,11 +29,13 @@ app.use(express.json());
 // if we're in production...
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
+  
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });  
+
+}
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
