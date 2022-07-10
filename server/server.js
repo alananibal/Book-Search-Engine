34ecb5ad7827c8ff39032f7946ee9ee1bef08a13
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-07-10 13:21:31 -0300

    server fix

--- server/server.js
+++ server/server.js
@@ -29,12 +29,14 @@ app.use(express.json());
 // if we're in production...
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });  
 
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});  
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
